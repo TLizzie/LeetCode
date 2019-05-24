52db35c4639027b549ac45534b9b13f6b0e814e5
Lizzieliang <ltang10@uci.edu>
2019-05-23 20:18:34 -0700

    add 275, 374

--- Array/src/leetcode/ShortestWordDistanceII.java
+++ Array/src/leetcode/ShortestWordDistanceII.java
@@ -1,12 +1,60 @@
 package leetcode;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
 /**
  * 244.Shortest Word DistanceII
  * 2019/5/23
  * Design a class which receives a list of words in the constructor,
  * and implements a method that takes two words word1 and word2 and return the shortest distance between these two words in the list.
  * Your method will be called repeatedly many times with different parameters.
+ *
+ * Using Hashmap
  */
-public class ShortestWordDistanceII {
+
+    public class ShortestWordDistanceII {
+
+    private HashMap<String, List<Integer>> map;
+    public ShortestWordDistanceII(String[] words) {
+        map = new HashMap<>();
+        for (int i = 0; i < words.length; i++) {
+            if (map.containsKey(words[i])) {
+                map.get(words[i]).add(i);
+            } else {
+                List<Integer> list = new ArrayList<>();
+                list.add(i);
+                map.put(words[i],list);
+            }
+
+        }
+    }
+
+    // time : O(m*n)
+    public int Shortest(String word1, String word2) {
+        List<Integer> l1 = map.get(word1);
+        List<Integer> l2 = map.get(word2);
+        int res = Integer.MAX_VALUE;
+        for (Integer i : l1) {
+            for (Integer j : l2) {
+                res = Math.min(res, Math.abs(j - i));
+            }
+        }
+        return res;
+    }
+
+    public int ShortestI(String word1, String word2) {
+        List<Integer> l1 = map.get(word1);
+        List<Integer> l2 = map.get(word2);
+        int res = Integer.MAX_VALUE;
+        int i = 0;
+        int j = 0;
+        while (i < l1.size() && j < l2.size()) {
+            res = Math.min(res, Math.abs(l1.get(i) - l2.get(j)));
+        }
+        return res;
+    }
+
 
 }
