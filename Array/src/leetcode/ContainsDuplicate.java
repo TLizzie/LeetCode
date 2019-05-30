7b3e7e9954f77b0327226b1a567f56b1635368c1
Lizzieliang <ltang10@uci.edu>
2019-05-30 01:43:31 -0700

    add 49, 217, 219

---
+++ Array/src/leetcode/ContainsDuplicate.java
@@ -0,0 +1,35 @@
+package leetcode;
+
+import java.util.Arrays;
+import java.util.HashSet;
+
+/**
+ * LeetCode 217
+ * 5/29
+ * Hashset API 基本都是返回的boolean型
+ *
+ */
+public class ContainsDuplicate {
+
+    //HashSet
+    public boolean containsDuplicate(int[] nums) {
+        HashSet<Integer> res = new HashSet<>();
+        for (int i = 0; i < nums.length; i++) {
+            if (!res.add(nums[i]))
+                return true;
+        }
+        return false;
+    }
+
+    //Sorting
+    public boolean containsDuplicate2(int[] nums) {
+        Arrays.sort(nums);
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] == nums[i - 1]) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+}
---
+++ Array/src/leetcode/ContainsDuplicatesII.java
@@ -0,0 +1,24 @@
+package leetcode;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.HashSet;
+
+/**
+ * LeetCode 219 Contains Duplicate II
+ * 5/29
+ * Hashset API 基本都是返回的boolean型
+ */
+public class ContainsDuplicatesII {
+    public boolean containsNearbyDuplicate(int[] nums, int k) {
+        HashMap<Integer, Integer> res = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            if (res.containsKey(nums[i])) {
+                if (i - res.get(nums[i]) <= k)
+                    return true;
+            }
+            res.put(nums[i], i);
+        }
+        return false;
+    }
+}
