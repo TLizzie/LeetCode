bcdfbbfeefaf7b84c434ecb80fe16eb5b6a04144
Lizzieliang <ltang10@uci.edu>
2019-06-20 00:28:57 -0700

    add 280, 28

--- LeetCode/String/ImplementstrStr.java
+++ LeetCode/String/ImplementstrStr.java
@@ -3,7 +3,24 @@ package LeetCode.String;
 /**
  * Leetcode 28. Implement strStr()
  * 6/19/2019
- *
+ * brute force
+ * time : O(m * n)
+ * space : O(1)
  */
 public class ImplementstrStr {
+    public int strStr(String haychack, String needle) {
+        if (needle == null || needle.length() == 0)
+            return 0;
+        for (int i = 0; i < haychack.length(); i++) {
+            if (needle.length() + i > haychack.length())
+                break;
+            for (int j = 0; j < needle.length(); j++) {
+                if (needle.charAt(j) != haychack.charAt(j + i))
+                    break;
+                if (j == needle.length() - 1)
+                    return i;
+            }
+        }
+        return -1;
+    }
 }
